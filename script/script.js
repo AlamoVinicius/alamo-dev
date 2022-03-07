5c9b33fc31e9a6a20637ad78e213b49bd480ada6
Álamo Vinícius <alamo.cma22@gmail.com>
2022-03-07 15:58:27 -0300

    adicionar campo envie uma menssagem

--- script/script.js
+++ script/script.js
@@ -1,5 +1,15 @@
 var typed = new Typed('#typed', {
-    stringsElement: '#typed-strings',
-    typeSpeed: 50,
-    loop: true
-  })
+  stringsElement: '#typed-strings',
+  typeSpeed: 50,
+  loop: true
+})
+
+
+
+let btn = document.querySelector("#sub-form")
+btn.addEventListener('click', clique)
+
+function clique() {
+  alert('Mensagem enviada com sucesso')
+}
+
